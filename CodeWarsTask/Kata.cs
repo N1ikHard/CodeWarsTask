dde1188b5b151fbd455cba0fc9afed65ee36b5d5
N1ikHard <N1ik@yandex.ru>
2021-05-18 14:17:33 +0300

    Добавьте файлы проекта.

---
+++ CodeWarsTask/Kata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTask
+{
+    class Kata
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            #region ТЗ
+            /*
+            Напишите функцию, которая принимает неотрицательное целое число (секунды)
+            в качестве входных данных и возвращает время в удобочитаемом формате (ЧЧ:ММ:СС)
+            HH = часы, дополненные 2 цифрами, диапазон: 00 - 99
+            ММ = минуты, заполненные до 2 цифр, диапазон: 00 - 59
+            SS = секунды, заполненные до 2 цифр, диапазон: 00 - 59
+            Максимальное время никогда не превышает 359999 (99:59:59)
+                
+            00:00:05", TimeFormat.GetReadableTime(5));
+            00:01:00", TimeFormat.GetReadableTime(60));
+            23:59:59", TimeFormat.GetReadableTime(86399));
+            99:59:59", TimeFormat.GetReadableTime(359999));
+            */
+            #endregion
+            //  int SS = seconds % 60;
+            //  seconds /= 60;
+            //  int MM = seconds%60;
+            //  seconds /= 60;
+            //  int HH = seconds%100;
+            // string result = ":"+(seconds % 60).ToString();
+            //  result = ((seconds/(60*60)%100).ToString()+":" + ((seconds / 60) % 60).ToString() + ":" + (seconds % 60).ToString());
+            //return HH.ToString() + ":" + MM.ToString() + ":" + SS.ToString();
+            return new string((seconds / (60 * 60) % 100).ToString("00") + ":" + ((seconds / 60) % 60).ToString("00") + ":" + (seconds % 60).ToString("00"));
+        }
+    }
+}
---
+++ CodeWarsTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeWarsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(Kata.GetReadableTime(0));
+           Console.WriteLine(Kata.GetReadableTime(5));
+            Console.WriteLine(Kata.GetReadableTime(60));
+            Console.WriteLine(Kata.GetReadableTime(86399));
+           
+           Console.WriteLine(Kata.GetReadableTime(359999));
+        }
+    }
+}
